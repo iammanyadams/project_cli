5e18ba550f113c1b295b29cff6ed79b601700a3d
Nathaniel Adam <iammanyadams@gmail.com>
2020-10-25 17:16:15 -0500

    ostruct

--- lib/damu/api.rb
+++ lib/damu/api.rb
@@ -5,8 +5,8 @@ class Datamuse < APICake::Base
    base_uri 'api.datamuse.com'
 
   def self.words(**args)
-    res = get('/words', query: args)
-    res.each_with_index{|r,i| Word.new = OpenStruct.new(r i)}
+    response  = get('/words', query: args)
+    response.each_with_index{|r,i| Word.new = OpenStruct.new(r i)}
     end
 
   def self.sug(**args)
